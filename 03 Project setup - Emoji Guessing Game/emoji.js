fd68c9c89785601d433853e38aa867633cbb57e9
priyankawani890 <60522378+priyankawani890@users.noreply.github.com>
2024-04-09 17:30:23 +0530

    Add files via upload

---
+++ 03 Project setup - Emoji Guessing Game/emoji.js
@@ -0,0 +1,63 @@
+const emojiDetails = [
+  { description: "Smiling face with sunglasses", emoji: "😎" },
+  { description: "Thumbs up", emoji: "👍" },
+  { description: "Heart eyes", emoji: "😍" },
+  { description: "Crying face", emoji: "😢" },
+  { description: "Party popper", emoji: "🎉" },
+  // Add more emoji descriptions here
+];
+
+  let currentEmojiIndex = 0;
+  let score = 0;
+  //
+
+
+  //
+  const guessInput = document.getElementById("guess-input");
+  const resultElement = document.getElementById("result");
+  const scoreElement = document.getElementById("score");
+
+  function displayEmoji() {
+    const descriptionElement = document.getElementById("description");
+    descriptionElement.textContent = emojiDetails[currentEmojiIndex].emoji;
+  }
+
+  function checkGuess() {
+    const guess = guessInput.value.trim().toLowerCase();
+    const correctEmoji = emojiDetails[currentEmojiIndex].description.trim().toLowerCase();
+
+    if (guess === correctEmoji) {
+      resultElement.textContent = "Correct!";
+      score++;
+    } else {
+      resultElement.textContent = "Wrong!";
+    }
+    console.log(score);
+    scoreElement.textContent = `Score: ${score}`;
+    guessInput.value = "";
+    guessInput.focus();
+    nextEmoji();
+  }
+
+  function nextEmoji() {
+    currentEmojiIndex++;
+
+    if (currentEmojiIndex === emojiDetails.length) {
+      currentEmojiIndex = 0;
+      score=0;
+    }
+
+    displayEmoji();
+  }
+
+  document
+    .getElementById("guess-input")
+    .addEventListener("keydown", (event) => {
+      if (event.key === "Enter") {
+        checkGuess();
+      }
+    });
+
+  document.addEventListener("DOMContentLoaded", () => {
+    displayEmoji();
+  });
